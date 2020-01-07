3a4c74fa38a8ec3b1550b2524ff85a0f118ee843
Omar Aziz <OmarAziz47@hotmail.com>
2020-01-08 00:08:36 +0300

    created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,9 @@
+console.log("Up and running!");
+
+let cardOne = "queen";
+let cardTwo = "queen";
+let cardThird = "king";
+let cardFour = "king";
+
+console.log("User flipped " + cardOne);
+console.log("User flipped " + cardThird);
